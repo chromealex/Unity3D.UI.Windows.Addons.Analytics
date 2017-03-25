30757dab197ddcd0edbd4988e96f5375b5f34d84
Alex <chrome.alex@gmail.com>
2017-03-26 00:34:19 +0300

    Initial commit

---
+++ Analytics/Analytics/Analytics/Services/Unity/Plugins/Heatmaps/Renderer/HeatmapSubmap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[ExecuteInEditMode]
+public class HeatmapSubmap : MonoBehaviour
+{
+
+    void Start()
+    {
+        GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        GetComponent<MeshRenderer>().receiveShadows = false;
+		#if UNITY_5_5_OR_NEWER
+		GetComponent<MeshRenderer>().lightProbeUsage = UnityEngine.Rendering.LightProbeUsage.Off;
+		#endif
+        GetComponent<MeshRenderer>().reflectionProbeUsage = UnityEngine.Rendering.ReflectionProbeUsage.Off;
+    }
+}
